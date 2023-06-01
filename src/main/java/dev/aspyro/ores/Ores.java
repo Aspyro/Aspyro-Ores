0ff990940af9f31adf20e253032c33bccfcccefb
Aspyro95 <damien@ghysweb.be>
2023-06-01 11:00:25 +0200

    Add first item : Aspyrium ingot

--- src/main/java/dev/aspyro/ores/Ores.java
+++ src/main/java/dev/aspyro/ores/Ores.java
@@ -1,8 +1,11 @@
 package dev.aspyro.ores;
 
 import com.mojang.logging.LogUtils;
+import dev.aspyro.ores.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.CreativeModeTabEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -23,13 +26,26 @@ public class Ores
     public Ores()
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(modEventBus);
+
         modEventBus.addListener(this::commonSetup);
         MinecraftForge.EVENT_BUS.register(this);
+
+        modEventBus.addListener(this::addCreative);
     }
 
     private void commonSetup(final FMLCommonSetupEvent event)
     {
 
+    }
+
+    private void addCreative(CreativeModeTabEvent.BuildContents event)
+    {
+        if(event.getTab() == CreativeModeTabs.INGREDIENTS)
+        {
+            event.accept(ModItems.ASPYRIUM_INGOT);
+        }
     }
 
     @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
---
+++ src/main/java/dev/aspyro/ores/item/ModItems.java
@@ -0,0 +1,21 @@
+package dev.aspyro.ores.item;
+
+import dev.aspyro.ores.Ores;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, Ores.MOD_ID);
+
+    public static final RegistryObject<Item> ASPYRIUM_INGOT = ITEMS.register("aspyrium_ingot",
+            () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus)
+    {
+        ITEMS.register(eventBus);
+    }
+}
